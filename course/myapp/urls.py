71cc4cd34465d787bd6f1c0df140915f5d3f2d0b
mouleeswarimurugan <mouleeswarimurugan@gmail.com>
2026-09-20 09:39:42 +0530

    Add Student Course Management System

--- course/myapp/urls.py
+++ course/myapp/urls.py
@@ -31,6 +31,6 @@ urlpatterns = [
     path("dashboard/",views.dashboard,name="dashboard"),
     path("signup/",views.signup, name="signup"),
     path("login/",views.login_view,name="login"),
-    path("home_page/",views.home,name="home"),
+    path("home/",views.home,name="home"),
     path("logout/", views.logout_view, name="logout")
 ]
